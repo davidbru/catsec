d37327bcc063809f4e43997dc96044c1be7b284b
David Brunnthaler <david@screencode.at>
2018-12-28 00:29:48 +0100

    favicons: add

--- src/index.php
+++ src/index.php
@@ -10,6 +10,16 @@ include_once('./scripts/data.php');
     <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1">
     <title>David Brunnthaler - Curriculum Vitae</title>
     <link rel="stylesheet" type="text/css" href="css/styles.min.css" media="all">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/img/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/img/favicon/site.webmanifest">
+    <link rel="mask-icon" href="/img/favicon/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="shortcut icon" href="/img/favicon/favicon.ico">
+    <meta name="msapplication-TileColor" content="#b91d47">
+    <meta name="msapplication-config" content="/img/favicon/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
 </head>
 <body>
 <header id="container_header" class="bg-dark">
