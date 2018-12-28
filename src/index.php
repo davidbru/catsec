ac18d0a5f0272815c748a605b8b190f89cd737a8
David Brunnthaler <david@screencode.at>
2018-12-28 22:36:31 +0100

    accessibility

--- src/index.php
+++ src/index.php
@@ -24,12 +24,14 @@ include_once('./scripts/data.php');
     <script defer src="/js/scripts.min.js"></script>
 </head>
 <body>
-<ul id="skiplinks" class="sr-only">
-    <li><a href="/#events">Zu den Veranstaltungen</a></li>
-    <li><a href="/#work">Zur Arbeit</a></li>
-    <li><a href="/#education">Zur Ausbildung</a></li>
-    <li><a href="/#podcasts">Zu den Podcasts</a></li>
-</ul>
+<nav>
+    <ul id="skiplinks" class="sr-only">
+        <li><a href="/#events">Zu den Veranstaltungen</a></li>
+        <li><a href="/#work">Zur Arbeit</a></li>
+        <li><a href="/#education">Zur Ausbildung</a></li>
+        <li><a href="/#podcasts">Zu den Podcasts</a></li>
+    </ul>
+</nav>
 
 <header id="container_header" class="bg-dark">
     <div class="container text-white">
@@ -44,7 +46,8 @@ include_once('./scripts/data.php');
     </div>
 </header>
 <main class="container">
-    <h2 class="mt-5" id="events">Veranstaltungen</h2>
+    <section>
+        <h2 class="mt-5" id="events">Veranstaltungen</h2>
         <?php
         $counter = 0;
         foreach($events AS $event) {
@@ -69,8 +72,10 @@ include_once('./scripts/data.php');
             $counter ++;
         }
         ?>
+    </section>
 
-    <h2 class="mt-5" id="work">Arbeit</h2>
+    <section>
+        <h2 class="mt-5" id="work">Arbeit</h2>
         <?php
         $counter = 0;
         foreach($works AS $work) {
@@ -94,74 +99,79 @@ include_once('./scripts/data.php');
             $counter ++;
         }
         ?>
+    </section>
 
-    <h2 class="mt-5" id="education">Ausbildung</h2>
-    <?php
-    $counter = 0;
-    foreach($educations AS $education) {
-        $lc_studyCourse = strtolower(str_replace(' ', '_', $education['study_course']));
-        echo '
-                <div class="row pt-1 border-top">
-                    <div class="col-12 col-sm-4 col-md-3 col-lg-2">
-                        '.formatDateRange($education['start_date'], (isset($education['end_date'])?$education['end_date']:NULL)).'
-                    </div>
-                    <div class="offset-1 col-11 offset-sm-0 col-sm-5 col-md-7 col-lg-5">'.
-                        wrapInExtLink($education['university'].' - '.$education['study_course'], (isset($education['url'])?$education['url']:NULL)).
-                        (isset($education['degree'])?'<br />'.$education['degree']:'').'
-                    </div>
-                    <div class="offset-2 col-10 offset-sm-0 col-sm-3 col-md-2 col-lg-5">
-                        '.(isset($education['location'])?$education['location']:'').'
-                    </div>
-                </div>
-                <div class="row pb-1">
-                    <div class="col-12 text-right"><a href="javascript:void(0)" class="ectsClickTarget">Zeige ECTS-Punkte</a></div>
-                </div>
-                ';
-
-        echo '<div class="row container_ects"><div class="col-12 border-top">';
-        $counterEctsOuter = 0;
-        foreach($education['ects'] AS $courseGroup) {
-            echo '<h3 class="mt-3 offset-1 offset-md-2 offset-lg-1">'.$courseGroup['title'].'</h3>';
-            $counterEctsInner = 0;
-            foreach($courseGroup['courses'] AS $course) {
-                $rowClasses = array('row', 'pt-1', 'pb-1', 'border-bottom', 'offset-1', 'offset-md-2', 'offset-lg-1');
-                if($counterEctsInner == 0) {
-                    $rowClasses[] = 'border-top';
-                }
-                if($counterEctsOuter === (sizeof($education['ects'])-1) && $counterEctsInner === (sizeof($courseGroup['courses'])-1)) {
-                    $rowClasses[] = 'mb-5';
-                }
-                echo '
-                    <div class="'.implode(' ', $rowClasses).'">
-                        <div class="col-1">'.$course['points'].'</div>
-                        <div class="col-11">'.$course['title'].'</div>
-                    </div>
-                ';
-                $counterEctsInner ++;
-            }
-            $counterEctsOuter ++;
-        }
-
-        echo '</div></div>';
-
-        if($counter !== 0) {
-            echo '<div class="row border-bottom"></div>';
-        }
-
-        $counter ++;
-    }
-    ?>
-
-    <h2 class="mt-5" id="podcasts">Podcasts</h2>
-    <ul class="podcasts row m-0 p-0">
+    <section>
+        <h2 class="mt-5" id="education">Ausbildung</h2>
         <?php
-        foreach($podcasts AS $podcast) {
-            echo '<li class="col-12 col-sm-6 col-md-4 col-lg-3 pb-3"><div>'.
-                wrapInExtLink('<img src="/img/'.$podcast['img'].'" alt="'.$podcast['title'].'" title="'.$podcast['title'].'" /><div class="text">'.$podcast['title'].'</div>', (isset($podcast['url'])?$podcast['url']:NULL)).
-                '</div></li>';
+        $counter = 0;
+        foreach($educations AS $education) {
+            $lc_studyCourse = strtolower(str_replace(' ', '_', $education['study_course']));
+            echo '
+                    <div class="row pt-1 border-top">
+                        <div class="col-12 col-sm-4 col-md-3 col-lg-2">
+                            '.formatDateRange($education['start_date'], (isset($education['end_date'])?$education['end_date']:NULL)).'
+                        </div>
+                        <div class="offset-1 col-11 offset-sm-0 col-sm-5 col-md-7 col-lg-5">'.
+                            wrapInExtLink($education['university'].' - '.$education['study_course'], (isset($education['url'])?$education['url']:NULL)).
+                            (isset($education['degree'])?'<br />'.$education['degree']:'').'
+                        </div>
+                        <div class="offset-2 col-10 offset-sm-0 col-sm-3 col-md-2 col-lg-5">
+                            '.(isset($education['location'])?$education['location']:'').'
+                        </div>
+                    </div>
+                    <div class="row pb-1">
+                        <div class="col-12 text-right"><a href="javascript:void(0)" class="ectsClickTarget">Zeige ECTS-Punkte</a></div>
+                    </div>
+                    ';
+
+            echo '<div class="row container_ects"><div class="col-12 border-top">';
+            $counterEctsOuter = 0;
+            foreach($education['ects'] AS $courseGroup) {
+                echo '<h3 class="mt-3 offset-1 offset-md-2 offset-lg-1">'.$courseGroup['title'].'</h3>';
+                $counterEctsInner = 0;
+                foreach($courseGroup['courses'] AS $course) {
+                    $rowClasses = array('row', 'pt-1', 'pb-1', 'border-bottom', 'offset-1', 'offset-md-2', 'offset-lg-1');
+                    if($counterEctsInner == 0) {
+                        $rowClasses[] = 'border-top';
+                    }
+                    if($counterEctsOuter === (sizeof($education['ects'])-1) && $counterEctsInner === (sizeof($courseGroup['courses'])-1)) {
+                        $rowClasses[] = 'mb-5';
+                    }
+                    echo '
+                        <div class="'.implode(' ', $rowClasses).'">
+                            <div class="col-1">'.$course['points'].'</div>
+                            <div class="col-11">'.$course['title'].'</div>
+                        </div>
+                    ';
+                    $counterEctsInner ++;
+                }
+                $counterEctsOuter ++;
+            }
+
+            echo '</div></div>';
+
+            if($counter !== 0) {
+                echo '<div class="row border-bottom"></div>';
+            }
+
+            $counter ++;
         }
         ?>
-    </ul>
+    </section>
+
+    <section>
+        <h2 class="mt-5" id="podcasts">Podcasts</h2>
+        <ul class="podcasts row m-0 p-0">
+            <?php
+            foreach($podcasts AS $podcast) {
+                echo '<li class="col-12 col-sm-6 col-md-4 col-lg-3 pb-3"><div>'.
+                    wrapInExtLink('<img src="/img/'.$podcast['img'].'" alt="'.$podcast['title'].' Podcast Cover Bild" title="'.$podcast['title'].' Podcast Cover Bild" /><div class="text">'.$podcast['title'].'</div>', (isset($podcast['url'])?$podcast['url']:NULL)).
+                    '</div></li>';
+            }
+            ?>
+        </ul>
+    </section>
 </main>
 </body>
 </html>
