872546953c3863e5eca9b4fb933dd7c8f2bac12d
David Brunnthaler <david@screencode.at>
2019-01-16 19:02:38 +0100

    refactor img folder structure

--- src/index.php
+++ src/index.php
@@ -194,7 +194,7 @@ include_once('./scripts/data.php');
             foreach($privateEducations AS $privateEducation) {
                 $altTag = $privateEducation['title'].' '.$privateEducation['type'].' Cover Bild';
                 echo '<li class="col-6 col-sm-4 col-md-3 col-lg-2 pb-3 type'.$privateEducation['type'].'"><div>'.
-                    Utils::wrapInExtLink('<img src="/img/'.$privateEducation['img'].'" alt="'.$altTag.'" title="'.$altTag.'" /><div class="text">'.$privateEducation['title'].' <small>'.$privateEducation['type'].'</small></div>', (isset($privateEducation['url'])?$privateEducation['url']:NULL)).
+                    Utils::wrapInExtLink('<img src="/img/education/'.$privateEducation['img'].'" alt="'.$altTag.'" title="'.$altTag.'" /><div class="text">'.$privateEducation['title'].' <small>'.$privateEducation['type'].'</small></div>', (isset($privateEducation['url'])?$privateEducation['url']:NULL)).
                     '</div></li>';
             }
             ?>
