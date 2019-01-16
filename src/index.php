690b039aacf4a247d55acc6ce497c287bcaabd41
David Brunnthaler <david@screencode.at>
2019-01-16 20:44:28 +0100

    main: refactor styling

--- src/index.php
+++ src/index.php
@@ -38,170 +38,183 @@ include_once('./scripts/data.php');
     <div class="container text-white">
         <div class="row">
             <div class="col-12 col-sm-2 d-flex flex-wrap align-items-center mt-3 mt-sm-0">
-                <img src="/img/logo_db.svg" alt="Logo David Brunnthaler" title="Logo David Brunnthaler" />
+                <img src="/img/logo_db.svg" alt="Logo David Brunnthaler" title="Logo David Brunnthaler"/>
             </div>
             <div class="col-12 col-sm-10">
-                <h1><small>Curriculum Vitae</small><br />David Brunnthaler</h1>
+                <h1>
+                    <small>Curriculum Vitae</small>
+                    <br/>David Brunnthaler
+                </h1>
             </div>
         </div>
     </div>
 </header>
-<main class="container">
-    <section id="work">
-        <h2 class="mt-5">Arbeit</h2>
-        <?php
-        $counter = 0;
-        foreach($works AS $work) {
-            $rowClasses = array('row', 'pt-1', 'pb-1', 'border-bottom');
-            if($counter == 0) {
-                $rowClasses[] = 'border-top';
-            }
-            echo '
-                <div class="'.implode(' ', $rowClasses).'">
+<main>
+    <section id="work" class="pt-5 pb-5">
+        <div class="container">
+            <h2>Arbeit</h2>
+            <?php
+            $counter = 0;
+            foreach ($works AS $work) {
+                $rowClasses = array('row', 'pt-1', 'pb-1', 'border-bottom');
+                if ($counter == 0) {
+                    $rowClasses[] = 'border-top';
+                }
+                echo '
+                <div class="' . implode(' ', $rowClasses) . '">
                     <div class="col-12 col-sm-4 col-md-3 col-lg-2">
-                        <small>'.Utils::formatDateRange($work['start_date'], (isset($work['end_date'])?$work['end_date']:NULL)).'</small>
+                        <small>' . Utils::formatDateRange($work['start_date'], (isset($work['end_date']) ? $work['end_date'] : NULL)) . '</small>
                     </div>
                     <div class="offset-1 col-11 offset-sm-0 col-sm-8 col-md-9 col-lg-10">
-                        '.Utils::wrapInExtLink($work['title'], (isset($work['url'])?$work['url']:NULL)).'
-                        '.(isset($work['addinfo'])?'<ul class="mb-0"><li>'.implode('</li><li>', $work['addinfo']).'</li></ul>':'').'
+                        ' . Utils::wrapInExtLink($work['title'], (isset($work['url']) ? $work['url'] : NULL)) . '
+                        ' . (isset($work['addinfo']) ? '<ul class="mb-0"><li>' . implode('</li><li>', $work['addinfo']) . '</li></ul>' : '') . '
                     </div>
                 </div>
                 ';
-            $counter ++;
-        }
-        ?>
+                $counter++;
+            }
+            ?>
+        </div>
     </section>
 
-    <section id="skills">
-        <h2 class="mt-5">Kompetenzen</h2>
-        <?php
-        $counter = 0;
-        foreach($skills AS $skillGroupKey => $skillGroupValue) {
-            $rowClasses = array('row', 'pt-1', 'pb-1', 'border-bottom');
-            if($counter == 0) {
-                $rowClasses[] = 'border-top';
-            }
-            echo '
-                <div class="'.implode(' ', $rowClasses).'">
+    <section id="skills" class="bg-light pt-5 pb-5">
+        <div class="container">
+            <h2>Kompetenzen</h2>
+            <?php
+            $counter = 0;
+            foreach ($skills AS $skillGroupKey => $skillGroupValue) {
+                $rowClasses = array('row', 'pt-1', 'pb-1', 'border-bottom');
+                if ($counter == 0) {
+                    $rowClasses[] = 'border-top';
+                }
+                echo '
+                <div class="' . implode(' ', $rowClasses) . '">
                     <div class="col-12 col-sm-4 col-md-3 col-lg-2">
-                        <small>'.$skillGroupKey.'</small>
+                        <small>' . $skillGroupKey . '</small>
                     </div>
                     <div class="offset-1 col-11 offset-sm-0 col-sm-8 col-md-9 col-lg-10">
-                        '.implode(', ', $skillGroupValue).'
+                        ' . implode(', ', $skillGroupValue) . '
                     </div>
                 </div>
                 ';
-            $counter ++;
-        }
-        ?>
+                $counter++;
+            }
+            ?>
+        </div>
     </section>
 
-    <section id="education">
-        <h2 class="mt-5">Ausbildung</h2>
-        <?php
-        $counter = 0;
-        foreach($educations AS $education) {
-            $lc_studyCourse = strtolower(str_replace(' ', '_', $education['study_course']));
-            echo '
+    <section id="education" class="pt-5 pb-5">
+        <div class="container">
+            <h2>Ausbildung</h2>
+            <?php
+            $counter = 0;
+            foreach ($educations AS $education) {
+                $lc_studyCourse = strtolower(str_replace(' ', '_', $education['study_course']));
+                echo '
                     <div class="row pt-1 border-top">
                         <div class="col-12 col-sm-4 col-md-3 col-lg-2">
-                            '.Utils::formatDateRange($education['start_date'], (isset($education['end_date'])?$education['end_date']:NULL)).'
+                            ' . Utils::formatDateRange($education['start_date'], (isset($education['end_date']) ? $education['end_date'] : NULL)) . '
                         </div>
-                        <div class="offset-1 col-11 offset-sm-0 col-sm-5 col-md-7 col-lg-5">'.
-                            Utils::wrapInExtLink($education['university'].' - '.$education['study_course'], (isset($education['url'])?$education['url']:NULL)).
-                            (isset($education['degree'])?'<br />'.$education['degree']:'').'
+                        <div class="offset-1 col-11 offset-sm-0 col-sm-5 col-md-7 col-lg-5">' .
+                    Utils::wrapInExtLink($education['university'] . ' - ' . $education['study_course'], (isset($education['url']) ? $education['url'] : NULL)) .
+                    (isset($education['degree']) ? '<br />' . $education['degree'] : '') . '
                         </div>
                         <div class="offset-2 col-10 offset-sm-0 col-sm-3 col-md-2 col-lg-5">
-                            '.(isset($education['location'])?$education['location']:'').'
+                            ' . (isset($education['location']) ? $education['location'] : '') . '
                         </div>
                     </div>
                     ';
-            if(isset($education['ects'])) {
-                echo '
+                if (isset($education['ects'])) {
+                    echo '
                         <div class="row pb-1">
                             <div class="col-12 text-right"><a href="javascript:void(0)" class="ectsClickTarget">Zeige ECTS-Punkte</a></div>
                         </div>
                         ';
 
-                echo '<div class="row container_ects"><div class="col-12 border-top">
+                    echo '<div class="row container_ects"><div class="col-12 border-top">
                 <h3 class="sr-only">ECTS-Punkte ' . $education['study_course'] . '</h3>';
-                $counterEctsOuter = 0;
-                foreach ($education['ects'] AS $courseGroup) {
-                    echo '<h4 class="mt-3 offset-1 offset-md-2 offset-lg-1">' . $courseGroup['title'] . '</h4>';
-                    $counterEctsInner = 0;
-                    foreach ($courseGroup['courses'] AS $course) {
-                        $rowClasses = array('row', 'pt-1', 'pb-1', 'border-bottom', 'offset-1', 'offset-md-2', 'offset-lg-1');
-                        if ($counterEctsInner == 0) {
-                            $rowClasses[] = 'border-top';
-                        }
-                        if ($counterEctsOuter === (sizeof($education['ects']) - 1) && $counterEctsInner === (sizeof($courseGroup['courses']) - 1)) {
-                            $rowClasses[] = 'mb-5';
-                        }
-                        echo '
+                    $counterEctsOuter = 0;
+                    foreach ($education['ects'] AS $courseGroup) {
+                        echo '<h4 class="mt-3 offset-1 offset-md-2 offset-lg-1">' . $courseGroup['title'] . '</h4>';
+                        $counterEctsInner = 0;
+                        foreach ($courseGroup['courses'] AS $course) {
+                            $rowClasses = array('row', 'pt-1', 'pb-1', 'border-bottom', 'offset-1', 'offset-md-2', 'offset-lg-1');
+                            if ($counterEctsInner == 0) {
+                                $rowClasses[] = 'border-top';
+                            }
+                            if ($counterEctsOuter === (sizeof($education['ects']) - 1) && $counterEctsInner === (sizeof($courseGroup['courses']) - 1)) {
+                                $rowClasses[] = 'mb-5';
+                            }
+                            echo '
                         <div class="' . implode(' ', $rowClasses) . '">
                             <div class="col-1">' . $course['points'] . '</div>
                             <div class="col-11">' . $course['title'] . '</div>
                         </div>
                     ';
-                        $counterEctsInner++;
+                            $counterEctsInner++;
+                        }
+                        $counterEctsOuter++;
                     }
-                    $counterEctsOuter++;
+                    echo '</div></div>';
                 }
-                echo '</div></div>';
-            }
 
-            if($counter !== 0) {
-                echo '<div class="row border-bottom"></div>';
-            }
+                if ($counter !== 0) {
+                    echo '<div class="row border-bottom"></div>';
+                }
 
-            $counter ++;
-        }
-        ?>
+                $counter++;
+            }
+            ?>
+        </div>
     </section>
 
-    <section id="events">
-        <h2 class="mt-5">Veranstaltungen</h2>
-        <?php
-        $counter = 0;
-        foreach($events AS $event) {
-            $rowClasses = array('row', 'pt-1', 'pb-1', 'border-bottom');
-            if($counter == 0) {
-                $rowClasses[] = 'border-top';
-            }
-            echo '
-                <div class="'.implode(' ', $rowClasses).'">
+    <section id="events" class="bg-light pt-5 pb-5">
+        <div class="container">
+            <h2>Veranstaltungen</h2>
+            <?php
+            $counter = 0;
+            foreach ($events AS $event) {
+                $rowClasses = array('row', 'pt-1', 'pb-1', 'border-bottom');
+                if ($counter == 0) {
+                    $rowClasses[] = 'border-top';
+                }
+                echo '
+                <div class="' . implode(' ', $rowClasses) . '">
                     <div class="col-12 col-sm-4 col-md-3 col-lg-2">
-                        <small>'.Utils::formatDateRange($event['start_date'], (isset($event['end_date'])?$event['end_date']:NULL)).'</small>
+                        <small>' . Utils::formatDateRange($event['start_date'], (isset($event['end_date']) ? $event['end_date'] : NULL)) . '</small>
                     </div>
                     <div class="offset-1 col-11 offset-sm-0 col-sm-5 col-md-7 col-lg-5">
-                        '.Utils::wrapInExtLink($event['title'], (isset($event['url'])?$event['url']:NULL)).'
-                        '.(isset($event['addinfo'])?'<small>'.$event['addinfo'].'</small>':'').'
+                        ' . Utils::wrapInExtLink($event['title'], (isset($event['url']) ? $event['url'] : NULL)) . '
+                        ' . (isset($event['addinfo']) ? '<small>' . $event['addinfo'] . '</small>' : '') . '
                     </div>
                     <div class="offset-2 col-10 offset-sm-0 col-sm-3 col-md-2 col-lg-5">
-                        '.$event['location'].'
+                        ' . $event['location'] . '
                     </div>
               </div>
                 ';
-            $counter ++;
-        }
-        ?>
-    </section>
-
-    <section id="privateEducation">
-        <h2 class="mt-5">Fortbildung</h2>
-        <div class="row pb-1 mb-3 border-bottom">
-            <div class="col-12">Digital & Analog</div>
-        </div>
-        <ul class="privateEducations row m-0 p-0">
-            <?php
-            foreach($privateEducations AS $privateEducation) {
-                $altTag = $privateEducation['title'].' Cover Bild';
-                echo '<li class="col-6 col-sm-4 col-md-3 col-lg-2 pb-3 type'.$privateEducation['type'].'"><div>'.
-                    Utils::wrapInExtLink('<img src="/img/education/'.$privateEducation['img'].'" alt="'.$altTag.'" title="'.$altTag.'" /><div class="text"><small>'.$privateEducation['type'].'</small><br />'.$privateEducation['title'].'</div>', (isset($privateEducation['url'])?$privateEducation['url']:NULL)).
-                    '</div></li>';
+                $counter++;
             }
             ?>
-        </ul>
+        </div>
+    </section>
+
+    <section id="privateEducation" class="pt-5 pb-5">
+        <div class="container">
+            <h2>Fortbildung</h2>
+            <div class="row pb-1 mb-3 border-bottom">
+                <div class="col-12">Digital & Analog</div>
+            </div>
+            <ul class="privateEducations row m-0 p-0">
+                <?php
+                foreach ($privateEducations AS $privateEducation) {
+                    $altTag = $privateEducation['title'] . ' Cover Bild';
+                    echo '<li class="col-6 col-sm-4 col-md-3 col-lg-2 pb-3 type' . $privateEducation['type'] . '"><div>' .
+                        Utils::wrapInExtLink('<img src="/img/education/' . $privateEducation['img'] . '" alt="' . $altTag . '" title="' . $altTag . '" /><div class="text"><small>' . $privateEducation['type'] . '</small><br />' . $privateEducation['title'] . '</div>', (isset($privateEducation['url']) ? $privateEducation['url'] : NULL)) .
+                        '</div></li>';
+                }
+                ?>
+            </ul>
+        </div>
     </section>
 </main>
 
@@ -209,8 +222,8 @@ include_once('./scripts/data.php');
     <div class="container text-white pt-4 pb-4">
         <div class="row">
             <div class="col-12 col-sm-6">
-                <strong>David Brunnthaler</strong><br />
-                <a href="mailto:david@screencode.at" class="text-white">david@screencode.at</a><br />
+                <strong>David Brunnthaler</strong><br/>
+                <a href="mailto:david@screencode.at" class="text-white">david@screencode.at</a><br/>
                 <a href="tel:+436649113740" class="text-white">+43 664 911 37 40</a>
             </div>
             <div class="col-12 col-sm-6 mt-3 m-sm-0 d-flex justify-content-end align-items-center">
